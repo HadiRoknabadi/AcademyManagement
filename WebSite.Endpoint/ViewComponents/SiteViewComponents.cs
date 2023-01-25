7000e2646615ab89332f97f4e403c9fdaa8d90fe
Hadi Roknabadi <dev.hadiroknabadi@gmail.com>
2023-01-25 20:32:58 +0330

    Transport Site Theme

---
+++ WebSite.Endpoint/ViewComponents/SiteViewComponents.cs
@@ -0,0 +1,41 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebSite.Endpoint.ViewComponents
+{
+    #region Site MetaTags
+
+    public class SiteMetaTags:ViewComponent
+    {
+       public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View("SiteMetaTags");
+        }
+    }
+
+    #endregion
+
+    #region Site Header
+
+    public class SiteHeader : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View("SiteHeader");
+        }
+    }
+
+    #endregion
+
+    #region Site Footer
+
+    public class SiteFooter : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View("SiteFooter");
+        }
+    }
+
+    #endregion
+}
