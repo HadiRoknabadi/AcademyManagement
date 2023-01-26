4516c7a9e9fce5c1baae2ff37d14550755875d94
Hadi Roknabadi <dev.hadiroknabadi@gmail.com>
2023-01-26 15:37:22 +0330

    Setup And Config [SMS Sender , Email Sender , Secret Manager]

---
+++ AcademyManagement.Infrastructure/Senders/ISenderService.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace AcademyManagement.Infrastructure.Senders
+{
+    public interface ISenderService
+    {
+        #region SendEmail
+
+        void SendEmail(string  to,string subject,string body);
+
+        #endregion
+
+        #region Send SMS
+        
+        Task SendSMS(string mobile,string message);
+
+        Task SendVerificationSMS(string mobile,string activationCode);
+
+        Task SendForgotPasswordVerificationSMS(string mobile,string activationCode);
+
+        Task SendUserPasswordSMS(string mobile,string password);
+
+        Task SendSuccessfullPayment(string mobile,string userName,string orderId);
+
+        string GetAPIKey(IConfiguration configuration);
+
+        #endregion
+    }
+}
---
+++ AcademyManagement.Infrastructure/Senders/SenderService.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Net.Mail;
+
+namespace AcademyManagement.Infrastructure.Senders
+{
+    public class SenderService : ISenderService
+    {
+        #region Constructor
+
+        private readonly IConfiguration _configuration;
+
+        public SenderService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        
+
+        #endregion
+
+        #region SendEmail
+
+        public void SendEmail(string to, string subject, string body)
+        {
+            MailMessage mail = new MailMessage();
+            SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
+            mail.From = new MailAddress("dev.hadiroknabadi@gmail.com", "عنوان");
+            mail.To.Add(to);
+            mail.Subject = subject;
+            mail.Body = body;
+            mail.IsBodyHtml = true;
+
+            //System.Net.Mail.Attachment attachment;
+            // attachment = new System.Net.Mail.Attachment("c:/textfile.txt");
+            // mail.Attachments.Add(attachment);
+
+            SmtpServer.Port = 587;
+            SmtpServer.Credentials = new System.Net.NetworkCredential("dev.hadiroknabadi@gmail.com", _configuration["EmailPassword"]);
+            SmtpServer.EnableSsl = true;
+
+            SmtpServer.Send(mail);
+        }
+
+        #endregion
+
+        #region Send SMS
+
+        public string GetAPIKey(IConfiguration configuration)
+        {
+            return  configuration["SmsAPIKey"];
+        }
+
+
+
+
+        public async Task SendSMS(string mobile,string message)
+        {
+            Kavenegar.KavenegarApi api = new Kavenegar.KavenegarApi(_configuration["SmsAPIKey"]);
+
+            await api.Send(_configuration["SmsSenderNumber"],mobile,message);
+        }
+
+
+        public async Task SendVerificationSMS(string mobile, string activationCode)
+        {
+            Kavenegar.KavenegarApi api = new Kavenegar.KavenegarApi(GetAPIKey(_configuration));
+            await api.VerifyLookup(mobile, activationCode, _configuration["VerificationSMSPatternName"]);
+        }
+
+        public async Task SendForgotPasswordVerificationSMS(string mobile,string activationCode)
+        {
+            Kavenegar.KavenegarApi api = new Kavenegar.KavenegarApi(GetAPIKey(_configuration));
+            await api.VerifyLookup(mobile, activationCode, _configuration["ForgotPasswordVerificationSMSPatternName"]);
+        }
+
+        public async Task SendUserPasswordSMS(string mobile,string password)
+        {
+            Kavenegar.KavenegarApi api = new Kavenegar.KavenegarApi(GetAPIKey(_configuration));
+            await api.VerifyLookup(mobile,password, _configuration["UserPasswordSMSPatternName"]);
+        }
+        public async Task SendSuccessfullPayment(string mobile,string userName,string orderId)
+        {
+            Kavenegar.KavenegarApi api = new Kavenegar.KavenegarApi(GetAPIKey(_configuration));
+            await api.VerifyLookup(mobile,userName,orderId,null, _configuration["SuccessfullPaymentPatternName"]);
+        }
+
+        #endregion
+
+    }
+}
