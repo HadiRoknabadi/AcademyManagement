65c8fd64a14cef43139c6c6d103a037e8d1a3533
Hadi Roknabadi <dev.hadiroknabadi@gmail.com>
2023-01-29 20:34:01 +0330

    Complete PreRegisteratonDTO Error Messages

--- AcademyManagement.Application/DTOs/Account/PreRegisterationDTO.cs
+++ AcademyManagement.Application/DTOs/Account/PreRegisterationDTO.cs
@@ -39,19 +39,19 @@ namespace AcademyManagement.Application.DTOs.Account
     {
         public PreRegisterationDTOValidator()
         {
-            RuleFor(p => p.Name).Length(1, 150).NotNull().WithMessage("لطفا نام خود را وارد کنید");
+            RuleFor(p => p.Name).Length(1, 150).WithMessage("نام نمی تواند کمتر از 1 و بیشتر از 150 کاراکتر باشد").NotNull().WithMessage("لطفا نام خود را وارد کنید");
 
-            RuleFor(p => p.Family).Length(1, 150).NotNull().WithMessage("لطفا نام خانوادگی خود را وارد کنید");
+            RuleFor(p => p.Family).Length(1, 150).WithMessage("نام خانوادگی نمی تواند کمتر از 1 و بیشتر از 150 کاراکتر باشد").NotNull().WithMessage("لطفا نام خانوادگی خود را وارد کنید");
 
-            RuleFor(p => p.PhoneNumber).Length(1, 50).NotNull().WithMessage("لطفا شماره موبایل خود را وارد کنید");
+            RuleFor(p => p.PhoneNumber).Length(11, 11).WithMessage("نام خانوادگی نمی تواند کمتر یا بیشتر از 11 کاراکتر باشد").NotNull().WithMessage("لطفا شماره موبایل خود را وارد کنید");
 
-            RuleFor(p => p.Grade).Length(1, 300).NotNull().WithMessage("لطفا مقطع تحصیلی خود را وارد کنید");
+            RuleFor(p => p.Grade).Length(1, 300).NotNull().WithMessage(" مقطع تحصیلی نمی تواند کمتر از 1 و بیشتر از 300 کاراکتر باشد").WithMessage("لطفا مقطع تحصیلی خود را وارد کنید");
 
             RuleFor(p => p.HowManyTermDidYouPass).InclusiveBetween(1,int.MaxValue).WithMessage("عدد وارد شده خارج از محدوده است");
 
-            RuleFor(p => p.BookNameReadInEnglishClass).Length(1, 300).WithMessage("نام کتاب نمی تواند بیشتر از 300 کاراکتر باشد");
+            RuleFor(p => p.BookNameReadInEnglishClass).Length(1, 300).WithMessage("نام کتاب نمی تواند کمتر از 1 و بیشتر از 300 کاراکتر باشد");
 
-            RuleFor(p => p.Description).Length(1, 300).WithMessage("متن توضیح دلخواه نمی تواند بیشتر از 300 کاراکتر باشد");
+            RuleFor(p => p.Description).Length(1, 300).WithMessage("متن توضیح دلخواه نمی تواند کمتر از 1 و بیشتر از 300 کاراکتر باشد");
         }
     }
 }
